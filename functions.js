e7fe5d6fda085c636fadd78f79901ad6dae572ce
Mayur Kulkarni <mayokulkarni@gmail.com>
2022-05-28 10:24:38 +0530

    Implemented td colour and position change

---
+++ functions.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',function(){
+document.querySelectorAll('td').forEach(function(td){
+    td.onclick=function(e){
+        td.addEventListener("click", e => e.target.style.background= "brown")
+        td.addEventListener("click", e => e.target.style.position= "relative");
+        td.addEventListener("click", e => e.target.style.bottom= "2px");
+        let counter =0;
+      
+        
+        
+    }
+
+});
+});
+    
