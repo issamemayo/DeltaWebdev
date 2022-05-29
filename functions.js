397896025b92ae433ff0d47abd5fa5b86c4c38eb
Mayur Kulkarni <mayokulkarni@gmail.com>
2022-05-29 17:31:23 +0530

    implemented getelement using variable rand

--- functions.js
+++ functions.js
@@ -3,15 +3,35 @@ const table= document.querySelector("table");
 table.addEventListener("click",function(e){
     if(e.target.nodeName =="TD"){
         e.target.style.backgroundColor= "brown";
-       
+        setTimeout(()=>{
+            if(e.target.nodeName =="TD"){
+            e.target.style.background="#a9c25d";
+            }
+            
+            },900)  ;
+    } 
+});
 
-        
+ setTimeout(function(){const appendedlist = [];
+while(appendedlist.length<=15){
+var rand= Math.floor(Math.random()*16) +1;
+if (appendedlist.includes(rand)== false){
+        appendedlist[appendedlist.length]=rand;
     }
-    setTimeout(()=>{
-        if(e.target.nodeName =="TD"){
-        e.target.style.background="#a9c25d";
-        }
-        
-        },700)
-});
-});
+let newtile=document.getElementById(`${rand}`);
+setTimeout(()=>{newtile.style.backgroundColor="brown";},900
+);
+    newtile.style.backgroundColor="#a9c25d";
+}});
+
+
+}
+
+
+)
+
+
+
+
+
+
