c538513025904c0c025f34b740f15e642249ef69
Mayur Kulkarni <mayokulkarni@gmail.com>
2022-05-29 12:48:19 +0530

    refined code with nodeName

--- functions.js
+++ functions.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded',function(){
-document.querySelectorAll('td').forEach(function(td){
-    td.onclick=function(e){
-        td.addEventListener("click", e => e.target.style.background= "brown")
-        td.addEventListener("click", e => e.target.style.position= "relative");
-        td.addEventListener("click", e => e.target.style.bottom= "2px");
-        setTimeout(()=>{
-        e.target.style.background="#a9c25d";
-        e.target.style.position= "relative";
-        e.target.style.top="2px";
-        },700)
-        
+const table= document.querySelector("table");
+table.addEventListener("click",function(e){
+    if(e.target.nodeName =="TD"){
+        e.target.style.backgroundColor= "brown";
+       
+
         
     }
-
+    setTimeout(()=>{
+        e.target.style.background="#a9c25d";
+       
+        
+        },700)
 });
 });
-    
