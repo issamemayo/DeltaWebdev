ed2b656469760071a42c28b41dfd93c965e689db
Mayur Kulkarni <mayokulkarni@gmail.com>
2022-05-30 12:50:46 +0530

    fixed bug

--- functions.js
+++ functions.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded',function(){
 const table= document.querySelector("table");
-table.addEventListener("click",function(e){
-    if(e.target.nodeName =="TD"){
+table.addEventListener("click",(e)=> keylightup(e));
+   
+        
+
+function keylightup(e){     
+    if(e.target.nodeName=="TD" && e.target.style.backgroundColor=="orange"){
         e.target.style.backgroundColor= "brown";
         setTimeout(()=>{
-            if(e.target.nodeName =="TD"){
-            e.target.style.background="#a9c25d";
-            }
-            
-            },900)  ;
-    } 
-});
+            e.target.style.backgroundColor="orange";
+        },1000)
+    }
+    }
+   
 
- setTimeout(function(){const appendedlist = [];
+
+
+ const appendedlist = [];
 while(appendedlist.length<=15){
 var rand= Math.floor(Math.random()*16) +1;
 if (appendedlist.includes(rand)== false){
@@ -22,12 +26,10 @@ let newtile=document.getElementById(`${rand}`);
 setTimeout(()=>{newtile.style.backgroundColor="brown";},900
 );
     newtile.style.backgroundColor="#a9c25d";
-}});
-
-
 }
 
 
+}
 )
 
 
