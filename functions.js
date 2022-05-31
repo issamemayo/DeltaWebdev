871f02e17d2c21d5b108f0de94bdf1c6fb8e4bf4
Mayur Kulkarni <mayokulkarni@gmail.com>
2022-05-31 19:20:33 +0530

    Added Start Button and fixed bugs

--- functions.js
+++ functions.js
@@ -1,46 +1,75 @@
 document.addEventListener('DOMContentLoaded',function(){
+    //
+    function keylightup(e){     
+        if(e.target.nodeName=="TD" && e.target.style.backgroundColor=="orange"){
+            e.target.style.backgroundColor= "brown";
+            e.target.style.position.bottom="5px";
+            e.target.style.position="relative";
+            setTimeout(()=>{
+                e.target.style.backgroundColor="orange";
+                e.target.style.position.top="5px";
+                e.target.style.position="relative";
+            },1000)
+        }
+        }
+        //
+        function lightup(token){
+            token.style.backgroundColor="brown";
+            setTimeout(()=>{
+                token.style.backgroundColor="orange";
+            },1000)
+        }
+        //
+    
 const table= document.querySelector("table");
 table.addEventListener("click",(e)=> keylightup(e));
    
-        
-
-function keylightup(e){     
-    if(e.target.nodeName=="TD" && e.target.style.backgroundColor=="orange"){
-        e.target.style.backgroundColor= "brown";
-        setTimeout(()=>{
-            e.target.style.backgroundColor="orange";
-        },1000)
-    }
-    }
-    function lightup(token){
-        token.style.backgroundColor="brown";
-        setTimeout(()=>{
-            token.style.backgroundColor="orange";
-        },1000)
-    }
-   
-
 
 
  const appendedlist = [];
- 
-while(appendedlist.length<=15){
+ while(appendedlist.length<=15){
 var rand= Math.floor(Math.random()*16) +1;
 if (appendedlist.includes(rand)== false){
  appendedlist[appendedlist.length]=rand;
-        console.log(rand);
-        let newtile=document.getElementById(`${rand}`);
-        setTimeout(()=>{
-    newtile.addEventListener("click",lightup(newtile));},1000*appendedlist.length);
+        console.log(`${rand}`);}
+          }
+          var i=0;
+          const checklist=[]
+          console.log(appendedlist);
+            appendedlist.forEach((valoo) =>{
+                  console.log(valoo);
+                var newtile=document.getElementById(`${valoo}`);
+                
+                var delay = appendedlist.indexOf(valoo)
+                checklist.push(valoo);
+                console.log(checklist);
+
+              
+                
+            
+
+    
+        setTimeout(() => {
+            lightup(newtile);
+            
+            
+        },2000*(delay+1));
+
+
+    })
+
+          
+
+     
 
     
 
 
     
-}
 
 
-}
+
+
 }
 )
 
