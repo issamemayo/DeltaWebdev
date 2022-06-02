6fc4337613699bbd5a5c6818ed3f5b281ae38fca
Mayur Kulkarni <mayokulkarni@gmail.com>
2022-06-02 19:01:29 +0530

    added styles

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded',function(){
     //
     function clicklightup(e){     
-        if(e.target.nodeName=="TD" && e.target.style.backgroundColor=="orange"&& enableClick==true){
-            e.target.style.backgroundColor= "brown";
+        if(e.target.nodeName=="TD" && e.target.style.backgroundColor=="orangered"){
+            e.target.style.backgroundColor= "#2F8CAB";
             e.target.style.position.bottom="5px";
             e.target.style.position="relative";
 
             setTimeout(()=>{
-                e.target.style.backgroundColor="orange";
+                e.target.style.backgroundColor="orangered";
                 e.target.style.position.top="5px";
                 e.target.style.position="relative";
             },1000)
@@ -15,32 +15,40 @@ document.addEventListener('DOMContentLoaded',function(){
         }
 //
         function lightup(token){
-            token.style.backgroundColor="brown";
+            token.style.backgroundColor="#2F8CAB";
             setTimeout(()=>{
-                token.style.backgroundColor="orange";
+                token.style.backgroundColor="orangered";
             },1000)
         }
 //
 function checker(e){
-        if (e.target.nodeName=="TD"){
-            console.log( e.target.id);
+        if (e.target.nodeName ==="TD"&& e.target.style.backgroundColor=="orangered"){
+            
+            e.target.style.backgroundColor= "#2F8CAB";
+            setTimeout(()=>{
+                e.target.style.backgroundColor="orangered";
+            let checkvalue = e.target.id;
+            console.log( checkvalue);
+        },1500);
+            
         }
     
 }
 
 //
-   var enableClick=false;
+document.querySelector('#start').onclick=gamebegin;
 
 function gamebegin(){
+
   
  const appendedlist = [];
+ const table= document.querySelector("table");
  while(appendedlist.length<=15){
 var rand= Math.floor(Math.random()*16) +1;
 if (appendedlist.includes(rand)== false){
  appendedlist[appendedlist.length]=rand;
         console.log(`${rand}`);}
           }
-          var i=0;
           console.log(appendedlist);
           const checklist=[]
             appendedlist.forEach((valoo) =>{
@@ -50,31 +58,28 @@ if (appendedlist.includes(rand)== false){
                 var delay = appendedlist.indexOf(valoo)
                 checklist.push(valoo);
                 console.log(checklist);
-                const table= document.querySelector("table");
-              table.addEventListener("click",(e)=> checker(e));
+                
               
                 
-            
+                
 
     
         setTimeout(() => {
             lightup(newtile);
-            console.log(newtile.id)
-            const table= document.querySelector("table");
-            enableClick=true;
-            table.addEventListener("click",(e)=> clicklightup(e));
-           
-            
-            
-            
+            console.log(newtile.id)     
         },3000*(delay+1));
+        console.log("hello");
+        table.addEventListener("click",(e)=> checker(e));
+        
+           
+        table.addEventListener("click",(e)=> clicklightup(e));
         
        
 
         
     })
 }
-document.querySelector('#start').onclick=gamebegin;
+
 
 
           
